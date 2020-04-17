b1979e281fcd07af3b65c7d1b3a5de2f1c4a2060
ozkanyldrm55 <58321853+ozkanyldrm55@users.noreply.github.com>
2020-04-17 22:38:38 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,15 +1,13 @@
-
 #include "stm32f4xx.h"
 #include "stm32f4_discovery.h"
 
-GPIO_InitTypeDef GPIO_InitStruct;
-TIM_TimeBaseInitTypeDef TIM_InitStruct;
-NVIC_InitTypeDef NVIC_InitStruct;
+GPIO_InitTypeDef 	  GPIO_InitStruct;
+TIM_TimeBaseInitTypeDef   TIM_InitStruct;
+NVIC_InitTypeDef     	  NVIC_InitStruct;
 
 int count ;
 
-void GPIO_Config(void)
-{
+void GPIO_Config(void) {
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD,ENABLE);
 
 	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
@@ -18,46 +16,43 @@ void GPIO_Config(void)
 	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
 	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
 
-	GPIO_Init(GPIOD,&GPIO_InitStruct);
+	GPIO_Init(GPIOD , &GPIO_InitStruct);
 }
 
-void TIM_Config(void)
-{
+void TIM_Config(void) {
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3,ENABLE);
-
-	TIM_InitStruct.TIM_ClockDivision = TIM_CKD_DIV1;
-	TIM_InitStruct.TIM_CounterMode = TIM_CounterMode_Up;
+	
+	/*   SN = ((periyod +1) * (prescaler +1)) / clock speed   */
+	TIM_InitStruct.TIM_ClockDivision = TIM_CKD_DIV1; 	// clock frekansını 1 degerine boluyoruz.
+	TIM_InitStruct.TIM_CounterMode = TIM_CounterMode_Up; 	// yukari dogru sayacak
 	TIM_InitStruct.TIM_Period = 1999;
 	TIM_InitStruct.TIM_Prescaler = 41999;
-	TIM_InitStruct.TIM_RepetitionCounter = 0;
+	TIM_InitStruct.TIM_RepetitionCounter = 0; 		// her tasmada tekrarlasin(kac tasmada tekralasin her zaman 1 falasi)
 
-	TIM_TimeBaseInit(TIM3,&TIM_InitStruct);
-
-	TIM_Cmd(TIM3,ENABLE);
-
+	TIM_TimeBaseInit(TIM3 , &TIM_InitStruct);
+	TIM_Cmd(TIM3 , ENABLE);
 }
 
-void NVIC_Config(void)
-{
-	NVIC_InitStruct.NVIC_IRQChannel = TIM3_IRQn;
+void NVIC_Config(void) { // interrupt yoneticisi
+	
+	// timer her 1999 degerine kadar saydiginda kesmeye gidecek ve Update interrupt flag (UIF) bayragi kalkacak
+	NVIC_InitStruct.NVIC_IRQChannel = TIM3_IRQn; 		 	// TIM3 kullanarak kesmeye(interrupt) gidecegini belirtir.
 	NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
-	NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0x00;
-	NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0x00;
+	NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0x00; 	// coklu interruptlarda gecerli oncelik icin 
+	NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0x00;		// coklu interruptlarda gecerli oncelik icin
 
 	TIM_ITConfig(TIM3,TIM_IT_Update,ENABLE);
-
 	NVIC_Init(&NVIC_InitStruct);
 }
 
-void TIME3_IRQHandler()
-{
+void TIME3_IRQHandler() { // her timer tasasi oldugunda yani 1999 oldugunda , Update interrupt flag (UIF) bayragi kalkacak ve fonksiyona gelecek 
 	GPIO_ToggleBits(GPIOD,GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15);
 
-	TIM_ClearITPendingBit(TIM3,TIM_IT_Update);
+	TIM_ClearITPendingBit(TIM3,TIM_IT_Update); // UIF flag i kalktiginda kendisi tekrardan pasif olmaz.
+						   // tekrar interrupt olsun istiyorsak UIF flag ini temizlemeliyiz.	
 }
 
-int main(void)
-{
+int main(void) {
 	GPIO_Config();
 	TIM_Config();
 	NVIC_Config();
@@ -65,7 +60,7 @@ int main(void)
 
   while (1)
   {
-	count = TIM_GetCounter(TIM3);
+	count = TIM_GetCounter(TIM3); // timer degerini izlemek icin olusturduk.(STM Studio dan izleyebiliriz.)
   }
 }
 
